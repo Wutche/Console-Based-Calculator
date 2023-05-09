9c0f688971a725bee4a25c347989f22ac9dca6bc
Sophia Okosodo <sophieokosodo@gmail.com>
2023-05-09 23:18:26 +0100

    first commit

--- src/main/java/org/example/CalculatorController.java
+++ src/main/java/org/example/CalculatorController.java
@@ -1,4 +1,128 @@
 package org.example;
 
-public class CalculatorController {
+public class CalculatorController implements CalculatorInterface{
+    @Override
+    public String Addition(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Subtraction(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Multiplication(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Division(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Power(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Square(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Cube(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String SquareRoot(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Round(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Ceiling(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Floor(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String MinValue(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String MaxValue(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Sin(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Cosine(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Asin(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Acos(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Atan(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Exponential(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Palindrome(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String ArmstrongNumber(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String PrimeNumber(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String Average(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String GCD(CalculatorModel calculatorModel) {
+        return null;
+    }
+
+    @Override
+    public String LCM(CalculatorModel calculatorModel) {
+        return null;
+    }
 }
--- src/main/java/org/example/CalculatorView.java
+++ src/main/java/org/example/CalculatorView.java
@@ -1,4 +1,52 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class CalculatorView {
+
+    public void CalcView(){
+        CalculatorModel calcModel = new CalculatorModel();
+        CalculatorController calcController = new CalculatorController();
+        Boolean exitApplication = false;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("*********************************************************************");
+        System.out.println("*                     CONSOLE BASED CALCULATOR                      *");
+        System.out.println("*********************************************************************");
+//        System.out.println();
+
+        while(!exitApplication){
+            System.out.println(">>> Enter an option below to continue.");
+            System.out.println("<1> Addition");
+            System.out.println("<2> Subtraction");
+            System.out.println("<3> Multiplication");
+            System.out.println("<4> Division");
+            System.out.println("<5> Power");
+            System.out.println("<6> Square");
+            System.out.println("<7> Cube");
+            System.out.println("<8> Square Root");
+            System.out.println("<9> Round");
+            System.out.println("<10> Ceiling");
+            System.out.println("<11> Floor");
+            System.out.println("<12> Minimum Value");
+            System.out.println("<13> Maximum Value");
+            System.out.println("<14> sin");
+            System.out.println("<15> cos");
+            System.out.println("<16> asin");
+            System.out.println("<17> acos");
+            System.out.println("<18> atan");
+            System.out.println("<19> exponential");
+            System.out.println("<20> Palindrome");
+            System.out.println("<21> Armstrong Number");
+            System.out.println("<22> Prime Number");
+            System.out.println("<23> Average");
+            System.out.println("<24> GCD");
+            System.out.println("<25> LCM");
+
+//            This is temporary(to avoid infinite loop)
+            exitApplication = true;
+        }
+
+    }
 }
