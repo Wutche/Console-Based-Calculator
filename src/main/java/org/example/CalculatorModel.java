5271b40083996581377f160c21447eec8315f69c
Sophia Okosodo <sophieokosodo@gmail.com>
2023-05-26 06:07:25 +0100

    removed uneccessary methods

--- src/main/java/org/example/CalculatorModel.java
+++ src/main/java/org/example/CalculatorModel.java
@@ -1,15 +1,13 @@
 package org.example;
 
 public class CalculatorModel {
-    private String expression;
     private int integers;
     private double operand1;
     private double operand2;
     private double[] array;
 
 
-    public CalculatorModel(String expression, int integers, double operand1, double operand2, double[] array) {
-        this.expression = expression;
+    public CalculatorModel( int integers, double operand1, double operand2, double[] array) {
         this.integers = integers;
         this.operand1 = operand1;
         this.operand2 = operand2;
@@ -18,13 +16,6 @@ public class CalculatorModel {
 
     public CalculatorModel() {}
 
-    public String getExpression() {
-        return expression;
-    }
-
-    public void setExpression(String expression) {
-        this.expression = expression;
-    }
 
     public int getIntegers() {
         return integers;
